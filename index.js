c5ae38741be30ab66727844295eee44d6a430ee2
ABDIRIZACK1 <abdirizack.adan@student.moringaschool.com>
2024-03-31 16:25:01 +0300

    arrowfunction

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression called divide
+const divide = function() {
+          return 2000 / 100;
+        };
+        
+        // Arrow function called square
+        const square = (num) => num * num;
+        
+        // Arrow function called add
+        const add = (num1, num2) => num1 + num2;
+        
+        console.log(divide()); 
+        console.log(square(5)); 
+        console.log(add(3, 7));
